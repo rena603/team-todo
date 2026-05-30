175fae2549f50a37e3e6fdca2b54416d43d3e970
山本怜奈 <rena@yamamotoreinanoMacBook-Air.local>
2026-05-30 11:46:22 +0900

    fix(sw): キャッシュバージョンをv2に更新して強制リフレッシュ

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'team-todo-v1';
+const CACHE = 'team-todo-v2';
 const STATIC = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
